46a89c6297446eea40adc2acdb60197b6ab60542
CoimbraPB <lucas.coimbra@pbcontabilidadeintegrada.com.br>
2025-05-23 12:26:21 -0300

    Cors upado

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const { Pool } = require("pg");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors()); // Liberar CORS para todas origens
 
 // Conexão com o PostgreSQL
 const pool = new Pool({
